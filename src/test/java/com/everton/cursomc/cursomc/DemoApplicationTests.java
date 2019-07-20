aca03eb91b4c38dbe9df45f830833d533efb82e0
Everton Rocha <evertonrps@outlook.com>
2019-07-20 08:17:46 -0300

    Projeto Criado

---
+++ src/test/java/com/everton/cursomc/cursomc/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everton.cursomc.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
