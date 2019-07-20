6277fb59232144ce733765668cc637e6bfb6316d
Everton Rocha <evertonrps@outlook.com>
2019-07-20 12:55:42 -0300

    Estado e cidade

--- src/main/java/com/everton/cursomc/DemoApplication.java
+++ src/main/java/com/everton/cursomc/DemoApplication.java
@@ -3,8 +3,12 @@ package com.everton.cursomc;
 import java.util.Arrays;
 
 import com.everton.cursomc.domain.Categoria;
+import com.everton.cursomc.domain.Cidade;
+import com.everton.cursomc.domain.Estado;
 import com.everton.cursomc.domain.Produto;
 import com.everton.cursomc.repositories.CategoriaRepository;
+import com.everton.cursomc.repositories.CidadeRepository;
+import com.everton.cursomc.repositories.EstadoRepository;
 import com.everton.cursomc.repositories.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +23,10 @@ public class DemoApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -40,9 +48,21 @@ public class DemoApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2, c3));
 	}
 
 }
