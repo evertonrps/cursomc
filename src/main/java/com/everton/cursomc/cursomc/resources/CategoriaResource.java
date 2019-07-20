ea0eb45cbf7040e194fb2f039f1999a9bae80103
Everton Rocha <evertonrps@outlook.com>
2019-07-20 08:34:54 -0300

    REST testado

---
+++ src/main/java/com/everton/cursomc/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,18 @@
+package com.everton.cursomc.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * CategoriaResource
+ */
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String listar() {
+        return "REST está funcionando";
+    }
+}
