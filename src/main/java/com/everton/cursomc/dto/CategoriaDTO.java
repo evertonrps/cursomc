803b86c16285dbee022d3cead98ab51c905c4592
Everton Rocha <evertonrps@outlook.com>
2019-07-21 09:26:19 -0300

    Paginacao com parametros opcionais

--- src/main/java/com/everton/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/everton/cursomc/dto/CategoriaDTO.java
@@ -4,9 +4,6 @@ import java.io.Serializable;
 
 import com.everton.cursomc.domain.Categoria;
 
-/**
- * CategoriaDTO
- */
 public class CategoriaDTO  implements Serializable{
 
 	private static final long serialVersionUID = 1L;
