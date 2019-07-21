65039851e4223105a1f77b8b7aad20530a6a01cf
Everton Rocha <evertonrps@outlook.com>
2019-07-21 08:49:53 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/everton/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/everton/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.everton.cursomc.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.everton.cursomc.services.exceptions.DataIntegrityException;
 import com.everton.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.http.HttpStatus;
@@ -16,5 +17,11 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis() );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis() );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
